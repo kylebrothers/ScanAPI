03c63d437334113af7751a4eab7fa87b5d28d8fc
kylebrothers <43184070+kylebrothers@users.noreply.github.com>
2025-09-21 17:44:51 -0400

    Update entrypoint.sh to debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+echo "Environment variables:"
+echo "SCANNER_BD_HOST=${SCANNER_BD_HOST}"
+echo "SCANNER_BD_PORT=${SCANNER_BD_PORT}"
+echo "SCANNER_69_HOST=${SCANNER_69_HOST}"
+echo "SCANNER_69_PORT=${SCANNER_69_PORT}"
+
 # Substitute environment variables in nginx config
-envsubst '${SCANNER_HOST} ${SCANNER_BD_PORT} ${SCANNER_69_PORT}' \
+envsubst '${SCANNER_BD_HOST} ${SCANNER_BD_PORT} ${SCANNER_69_HOST} ${SCANNER_69_PORT}' \
     < /etc/nginx/conf.d/default.conf.template \
     > /etc/nginx/conf.d/default.conf
+
+echo "Generated config:"
+cat /etc/nginx/conf.d/default.conf
 
 # Start OpenResty
 exec "$@"
